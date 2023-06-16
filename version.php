24fba485f0b525023ad74dec40b2d3d098f98906
Ahmed Belhaj <58787184+Theemiss@users.noreply.github.com>
2023-06-16 16:34:00 +0100

    Update version.php

--- version.php
+++ version.php
@@ -3,5 +3,5 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version  = 2016021600;   // The (date) version of this plugin
-$plugin->requires = 2011021900;   // Requires this Moodle version
+$plugin->requires = 2019111200;
 $plugin->component = 'local_oauth';
